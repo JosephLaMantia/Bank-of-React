2895119595d3b94f36b9c0701944b77c16506b33
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-18 23:03:45 -0500

    Created template for bank-of-react assignment, along with a navbar.

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div className="nav-component">
+                <button className="nav-button"><Link to="./">Home</Link></button>
+                <button className="nav-button"><Link to="./userProfile">Profile</Link></button>
+                <button className="nav-button"><Link to="./login">Login</Link></button>
+            </div>
+        )
+    }
+}
+
+
